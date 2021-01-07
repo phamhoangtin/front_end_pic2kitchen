0a2f447af918df284e2ecd89fff33f2c3a2ca1fa
Hoang Luan <nhluan.12tin@gmail.com>
2021-01-07 22:42:52 +0700

    Add login handle

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
-import axios from 'axios'
+//import axios from 'axios'
 function Header(props) {
-
-  const getMethod = () => {
-    axios.get('http://127.0.0.1:8000/hello/')
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+  let isLoggedIn = false
+  if (localStorage.getItem('userInfo') !== null){
+   isLoggedIn = true
   }
+  const handleLogout=() =>{
+    localStorage.removeItem("userInfo")
+    isLoggedIn = false
+    window.location.href = "/"
+  }
+  console.log('login ' + isLoggedIn);
   return (
     <Container fluid className="sticky-header px-0">
       <navbar className="col-md-12 px-0 ">
@@ -29,9 +27,14 @@ function Header(props) {
         </div>
         <div>
           <a className="navlink" href="/dishes" > Dishes </a>
-          <a className="navlink" href="/about" onClick={getMethod} >About</a>
-          <button className="navlink" onClick={props.isLogin.bind(true)} variant="light">Log in</button>
-          <button className="navlink" onClick={props.isRegis.bind(true)} variant="light">Sign up</button>
+          <a className="navlink" href="/about" >About</a>
+          {isLoggedIn 
+            ? <button className="navlink" variant="light">{localStorage.userInfo}</button>
+            : <button className="navlink" onClick={props.isLogin.bind(true)} variant="light">Log in</button>}
+          {isLoggedIn
+            ? <button className="navlink" onClick={handleLogout.bind()} variant="light">Log out</button>
+            : <button className="navlink" onClick={props.isRegis.bind(true)} variant="light">Sign up</button>}  
+            
         </div>
       </navbar>
     </Container>
